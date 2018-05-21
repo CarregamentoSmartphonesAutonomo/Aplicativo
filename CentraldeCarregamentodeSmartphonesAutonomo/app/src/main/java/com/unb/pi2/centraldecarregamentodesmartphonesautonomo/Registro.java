8529fff6a1c3621cea06362af5ec0207c1d02b03
Kleber Brito <kleberbritomoreira10@gmail.com>
2018-05-21 12:31:21 -0300

    Creation class to register users

---
+++ CentraldeCarregamentodeSmartphonesAutonomo/app/src/main/java/com/unb/pi2/centraldecarregamentodesmartphonesautonomo/Registro.java
@@ -0,0 +1,17 @@
+package com.unb.pi2.centraldecarregamentodesmartphonesautonomo;
+
+public class Registro {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Registro(String nome, String endereco, String telefone)
+    {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    public String getNome() { return nome; }
+    public String getTelefone() { return telefone; }
+    public String getEndereco() { return endereco; }
+}
