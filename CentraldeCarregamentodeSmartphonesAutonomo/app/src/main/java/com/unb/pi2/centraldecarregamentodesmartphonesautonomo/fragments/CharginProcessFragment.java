df8c6790526cabe05932e1e146d95000ef46e69f
Lucas Rufino <lucaslrt@outlook.com>
2018-06-19 12:55:54 -0300

    Changing charging process.

--- CentraldeCarregamentodeSmartphonesAutonomo/app/src/main/java/com/unb/pi2/centraldecarregamentodesmartphonesautonomo/fragments/CharginProcessFragment.java
+++ CentraldeCarregamentodeSmartphonesAutonomo/app/src/main/java/com/unb/pi2/centraldecarregamentodesmartphonesautonomo/fragments/CharginProcessFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.unb.pi2.centraldecarregamentodesmartphonesautonomo.R;
 import com.unb.pi2.centraldecarregamentodesmartphonesautonomo.Utils.RCClient;
@@ -18,16 +19,15 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CharginProcessFragment extends Fragment {
     private final String TAG = CharginProcessFragment.class.getSimpleName();
 
-    RCClient rcClient;
-    int pulseWidth = 1400;
-    private OnMessageReceived mMessageListener = null;
+    private RCClient rcClient;
+    private Button cabin1Button;
+    private Button cabin2Button;
+    private Button cabin3Button;
 
     Thread thread = new Thread(new Runnable() {
         @TargetApi(Build.VERSION_CODES.KITKAT)
@@ -37,7 +37,7 @@ public class CharginProcessFragment extends Fragment {
             rcClient = new RCClient();
 
             // Send first command to raspberry
-            rcClient.sendChargeStep("1");
+            rcClient.sendChargeStep("1|null");
 
             Log.d(TAG, "-> Receiving data...");
 
@@ -52,35 +52,60 @@ public class CharginProcessFragment extends Fragment {
 
                 //in this while the client listens for the messages sent by the server
                 while (isRunning) {
-                    String serverCommand = in.readLine();
-                    Log.d(TAG, "Server command -> " + serverCommand);
+                    String serverCommandResponse = in.readLine();
+                    Log.d(TAG, "Server command -> " + serverCommandResponse);
                     String serverData = in.readLine();
 
-                    switch (serverCommand){
+                    switch (serverCommandResponse){
+                        // Receiving free cabin numbers
                         case "1":
                             Log.d(TAG, "Server data -> " + serverData);
                             Log.d(TAG, "Sending second command -> 2");
-                            rcClient.sendChargeStep("2");
+
+                            /*// Separar a string para verificar quais cabines estão livres
+                            ArrayList<Integer> freeCabins = new ArrayList<>();
+                            setFreeCabins(freeCabins);*/
+
+                            rcClient.sendChargeStep("2|01");
+
+
                             continue;
+
+                        // Receiving if the facial recognition was successful (True or False)
                         case "2":
                             Log.d(TAG, "Server data -> " + serverData);
                             Log.d(TAG, "Sending third command -> 3");
-                            rcClient.sendChargeStep("3");
+
+                            // Seding data to opening the cab (name, cpf, cabin number)
+                            rcClient.sendChargeStep("3|Dario,01,2");
                             continue;
+
+                        // Receiving the cellphone insertion in the cabin was successful
                         case "3":
                             Log.d(TAG, "Server data -> " + serverData);
                             Log.d(TAG, "Sending forth command -> 4");
-                            rcClient.sendChargeStep("4");
+
+                            // Confirm the cabin confirmation
+                            rcClient.sendChargeStep("4|null");
                             continue;
+
+                        // Receiving if the confirmation was successful (true or false)
                         case "4":
                             Log.d(TAG, "Server data -> " + serverData);
                             Log.d(TAG, "Sending fith command -> 5");
-                            //rcClient.sendChargeStep("5\n");
+
+                            // Removing smartphone (sending cabin number)
+                            rcClient.sendChargeStep("5|2");
                             continue;
+
+                        // Receiving if the removal was successful (true or false)
                         case "5":
                             Log.d(TAG, "Server data -> " + serverData);
                             Log.d(TAG, "Finishing connection");
-                            //rcClient.sendChargeStep("ahauhau");
+
+                            // Finishing connection.
+                            rcClient.sendChargeStep("f|null");
+                            rcClient.closeUp();
                             break;
                         default:
                             Log.d(TAG, "Command not found.");
@@ -98,49 +123,52 @@ public class CharginProcessFragment extends Fragment {
                 // after it is closed, which means a new socket instance has to be created.
                 rcClient.closeUp();
             }
-
-            /*dIn = new DataInputStream(rcClient.getSocket().getInputStream());
-                    byte messageType = dIn.readByte();
-
-                    Log.d(TAG, "Data received -> " + String.valueOf(messageType));
-                    switch(messageType)
-                    {
-                        case 1: // Type A
-                            System.out.println("Message A: " + dIn.readUTF());
-                            break;
-                        case 2: // Type B
-                            System.out.println("Message B: " + dIn.readUTF());
-                            break;
-                        case 3: // Type C
-                            System.out.println("Message C [1]: " + dIn.readUTF());
-                            System.out.println("Message C [2]: " + dIn.readUTF());
-                            break;
-                        default:
-                            done = true;
-                    }
-                }
-
-                dIn.close();
-                rcClient.closeUp();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }*/
         }
     });
+    
+    private void setFreeCabins(ArrayList<Integer> freeCabins){
+        // Verify if the buttons were clicked
+        cabin1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sending data to obtain photos from facial recognition
+                rcClient.sendChargeStep("1|01");
+            }
+        });
 
-    public interface OnMessageReceived {
-        void messageReceived(String message);
+        cabin2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sending data to obtain photos from facial recognition
+                rcClient.sendChargeStep("2|01");
+            }
+        });
+
+        cabin3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sending data to obtain photos from facial recognition
+                rcClient.sendChargeStep("3|01");
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chargin_process, container, false);
+
+        // Instanciating view elements
+        cabin1Button = view.findViewById(R.id.cabin1_bt);
+        cabin2Button = view.findViewById(R.id.cabin2_bt);
+        cabin3Button = view.findViewById(R.id.cabin3_bt);
+
+
 
         Log.d("ChargingProcessFragment","onCreateView -> starting thread...");
         thread.start();
 
-        return inflater.inflate(R.layout.fragment_chargin_process, container, false);
+        return view;
     }
 }
